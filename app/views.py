39fac6b2ba1c5c70b0517c24813a9684345716d2
rdobson94 <r_dobson@live.com>
2017-02-05 21:40:39 +0000

    Added Profile,Route & Time

--- app/views.py
+++ app/views.py
@@ -4,19 +4,29 @@ Jinja2 Documentation:    http://jinja.pocoo.org/2/documentation/
 Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 This file creates your application.
 """
-
 from app import app
 from flask import render_template, request, redirect, url_for
+import time
+
 
 
 ###
 # Routing for your application.
-###
+
 
 @app.route('/')
 def home():
     """Render website's home page."""
     return render_template('home.html')
+    
+@app.route('/profile/')
+def profile():
+   return render_template('profile.html', now = timeinfo())
+   
+def timeinfo():
+    now = time.strftime("%a, %d %b %Y ")
+    return ("Today's date is  %s"  % now )   
+   
 
 
 @app.route('/about/')
@@ -24,11 +34,12 @@ def about():
     """Render the website's about page."""
     return render_template('about.html', name="Mary Jane")
 
+   
+    
 
 ###
 # The functions below should be applicable to all Flask apps.
 ###
-
 @app.route('/<file_name>.txt')
 def send_text_file(file_name):
     """Send your static text file."""
@@ -51,7 +62,10 @@ def add_header(response):
 def page_not_found(error):
     """Custom 404 page."""
     return render_template('404.html'), 404
+    
 
 
 if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port="8080")
+    
+ 
